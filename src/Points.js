68d28812976bf53c3cc292f4e50fc978c37d35c9
Chi Pham <chi.pham@vpn73221.vpn.mcgill.ca>
2019-02-08 13:40:38 -0500

    checklist page message at bottom

--- src/Points.js
+++ src/Points.js
@@ -9,12 +9,12 @@ import './Points.css';
 
 const certification = score => {
   if (score < 0.5)
-    return 'Unfortunately, this score will not qualify your event for a certification ranking. Submitting anyway will put you in contact with our consultants, who can help you get there!';
+    return 'Based on the items you selected, your event might not qualify for a sustainable certification yet. Our team of consultants can help you implement more actions to get there. Please click the Next button below and make sure to click Submit on the next page to send us your request!';
   if (score < 0.75)
-    return 'This score qualifies your event for a bronze certification, well done!';
+    return 'Based on the items you selected, your event might qualify for a bronze certification. Our team of consultants will meet with you to verify your score and give you a final certification. Please click the Next button below and make sure to click Submit on the next page to send us your request!';
   if (score < 0.9)
-    return 'This score qualifies your event for a silver certification, well done!';
-  return 'This score qualifies your event for a gold certification, well done!';
+    return 'Based on the items you selected, your event might qualify for a silver certification. Our team of consultants will meet with you to verify your score and give you a final certification. Please click the Next button below and make sure to click Submit on the next page to send us your request!';
+  return 'Based on the items you selected, your event might qualify for a gold certification. Our team of consultants will meet with you to verify your score and give you a final certification. Please click the Next button below and make sure to click Submit on the next page to send us your request!';
 };
 
 const Points = ({ pointsNumerator, pointsDenominator }) => (
